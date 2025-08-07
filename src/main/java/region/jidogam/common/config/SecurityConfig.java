9a001540f72d9c9ca62d816ad7a45b2332567a7e
yudility <yudility@gmail.com>
2025-08-08 00:50:49 +0900

    chore: Security Config 추가

---
+++ src/main/java/region/jidogam/common/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package region.jidogam.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(AbstractHttpConfigurer::disable) // REST API
+
+        .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+        .authorizeHttpRequests(auth -> auth
+            .anyRequest().permitAll());
+
+    return http.build();
+  }
+}
