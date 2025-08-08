7d7fa314234e748b1b88be92e8a79fdccb72f49c
yerimYeon <99879845+yinneu@users.noreply.github.com>
2025-08-08 11:52:28 +0900

    feat: area 테이블 복합 unique 키 추가

--- src/main/java/region/jidogam/domain/area/entity/Area.java
+++ src/main/java/region/jidogam/domain/area/entity/Area.java
@@ -3,6 +3,7 @@ package region.jidogam.domain.area.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,7 +13,11 @@ import region.jidogam.common.entity.BaseEntity;
 import region.jidogam.common.entity.BaseUpdatableEntity;
 
 @Entity
-@Table(name = "areas")
+@Table(
+  name = "areas", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"sido", "sigungu"})
+  }
+)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
