5dfb6b453ab33bde33a71a2a760f620e7a6cbca2
yudility <yudility@gmail.com>
2025-08-22 16:31:08 +0900

    feat: UserNotFoundException에 email 확인 메서드 추가
    - 사용자 조회 시 이메일 기반 예외 처리 메서드 추가
    - 기존 id 기반 예외 메시지 및 새 메시지를 한글로 수정하여 가독성 향상

--- src/main/java/region/jidogam/domain/user/exception/UserNotFoundException.java
+++ src/main/java/region/jidogam/domain/user/exception/UserNotFoundException.java
@@ -9,7 +9,11 @@ public class UserNotFoundException extends UserException {
   }
 
   public static UserNotFoundException withId(UUID id) {
-    return new UserNotFoundException(id + " not found");
+    return new UserNotFoundException("id = '"+ id + "'인 사용자가 존재하지 않습니다.");
+  }
+
+  public static UserNotFoundException withEmail(String email) {
+    return new UserNotFoundException("email = '"+ email + "'인 사용자가 존재하지 않습니다. ");
   }
 
 }
