be82ca1c2f2b67ae6469832a97f790262e1aafc1
yudility <yudility@gmail.com>
2025-08-22 16:35:16 +0900

    feat: Spring Security 설정에 JwtAuthenticationFilter 추가
    - JwtAuthenticationFilter를 Spring Security 필터 체인에 등록하여 JWT 인증 기능 구현.
    - 로그인 및 사용자 등록 요청은 인증 없이 허용하고, 그 외 요청은 USER 권한 필요하도록 설정 수정.
    - REST API 기반 세션 정책 Stateless로 유지.

--- src/main/java/region/jidogam/common/config/SecurityConfig.java
+++ src/main/java/region/jidogam/common/config/SecurityConfig.java
@@ -1,14 +1,24 @@
 package region.jidogam.common.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import region.jidogam.infrastructure.jwt.JwtProvider;
+import region.jidogam.infrastructure.security.JidogamUserDetailsService;
+import region.jidogam.infrastructure.security.JwtAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+  private final JwtProvider jwtProvider;
+  private final JidogamUserDetailsService jidogamUserDetailsService;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -16,10 +26,17 @@ public class SecurityConfig {
         .csrf(AbstractHttpConfigurer::disable) // REST API
 
         .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
         .authorizeHttpRequests(auth -> auth
-            .anyRequest().permitAll());
+            .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+            .anyRequest().hasRole("USER"))
+        //.anyRequest().permitAll()) // 개발용
+
+        .addFilterBefore(
+            new JwtAuthenticationFilter(jwtProvider, jidogamUserDetailsService),
+            UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
