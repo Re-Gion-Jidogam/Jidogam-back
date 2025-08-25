ec92885eaef9abeca2952ce0a82748e36daeb28b
yudility <yudility@gmail.com>
2025-08-25 19:12:54 +0900

    feat: 이메일 인증 코드 엔티티 및 테이블 추가
    - 이메일 인증 코드를 저장하기 위한 엔티티와 데이터베이스 테이블을 추가

---
+++ src/main/java/region/jidogam/domain/auth/entity/EmailAuthCode.java
@@ -0,0 +1,35 @@
+package region.jidogam.domain.auth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import region.jidogam.common.entity.BaseEntity;
+
+@Entity
+@Table(name = "email_auth_codes")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class EmailAuthCode extends BaseEntity {
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String code;
+
+  @Column(nullable = false)
+  private LocalDateTime expiresAt;
+
+  @Column(nullable = false)
+  @Builder.Default
+  private Boolean used = false;
+
+}
