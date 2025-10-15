a44e5d1a96b7dda6d97423c14385643e542c8577
yudility <yudility@gmail.com>
2025-10-15 15:00:11 +0900

    refactor: UserGuidebookSearchRequest 리팩토링 및 필드 추가
    DTO 필드를 수정하고, null 처리 기본값 로직을 추가했습니다. 검색 키워드, 정렬, 필터 등 스키마 설명을 개선하고 사용자 지정 필터를 지원하도록 필드를 확장했습니다.

--- src/main/java/region/jidogam/domain/user/dto/UserGuidebookSearchRequest.java
+++ src/main/java/region/jidogam/domain/user/dto/UserGuidebookSearchRequest.java
@@ -1,27 +1,38 @@
 package region.jidogam.domain.user.dto;
 
-import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import lombok.Builder;
+import org.springdoc.core.annotations.ParameterObject;
+import region.jidogam.common.dto.SortDirection;
+import region.jidogam.domain.guidebook.dto.GuidebookFilter;
 
-@Builder
+@ParameterObject
 public record UserGuidebookSearchRequest(
-    @Parameter(description = "검색어", example = "밥")
-    String searchKeyword,
+    @Schema(description = "검색 키워드", example = "밥")
+    String keyword,
 
-    @Parameter(description = "정렬 기준(생성일, 수정일)")
-    String sortBy,
+    @Schema(description = "정렬 기준(CREATED_AT, UPDATED_AT)", defaultValue = "CREATED_AT", nullable = true)
+    UserGuideBookSortBy sortBy,
 
-    @Parameter(description = "정렬 방향(ASC, DESC)", example = "DESC")
-    String sortDirection,
+    @Schema(description = "정렬 방향(ASC, DESC)", example = "DESC")
+    SortDirection sortDirection,
 
-    @Parameter(description = "커서", example = "")
+    @Schema(description = "커서")
     String cursor,
 
-    @Parameter(description = "조회 크기(개수)", example = "20")
+    @Schema(description = "조회 크기(개수)", example = "20")
     @Max(value = 100)
     @Min(value = 1)
-    Integer size
+    Integer limit,
+
+    @Schema(description = "필터 타입", nullable = true)
+    GuidebookFilter filter
 ) {
+
+  public UserGuidebookSearchRequest {
+    limit = limit == null ? 20 : limit;
+    sortBy = sortBy == null ? UserGuideBookSortBy.CREATED_AT : sortBy;
+    sortDirection = sortDirection == null ? SortDirection.DESC : sortDirection;
+  }
 }
