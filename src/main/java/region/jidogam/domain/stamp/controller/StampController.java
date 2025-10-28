d4a4b53d6e4df3263aeaf33251dd8cf8d580c781
yerimYeon <99879845+yinneu@users.noreply.github.com>
2025-10-29 00:39:00 +0900

    refactor: StampController parameter userId -> principal 변경

--- src/main/java/region/jidogam/domain/stamp/controller/StampController.java
+++ src/main/java/region/jidogam/domain/stamp/controller/StampController.java
@@ -4,15 +4,16 @@ import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import region.jidogam.domain.stamp.dto.PlaceStampRequest;
 import region.jidogam.domain.stamp.service.StampService;
+import region.jidogam.infrastructure.security.JidogamUserDetails;
 
 @RestController
 @RequestMapping("/api/stamps")
@@ -23,19 +24,19 @@ public class StampController {
 
   @PostMapping
   public ResponseEntity<Void> stampPlace(
-    @Valid @RequestBody PlaceStampRequest request,
-    @RequestParam(name = "userId") UUID userId // 임시
+      @Valid @RequestBody PlaceStampRequest request,
+      @AuthenticationPrincipal JidogamUserDetails principal
   ) {
-    stampService.stampPlace(request, userId);
+    stampService.stampPlace(request, principal.getId());
     return ResponseEntity.ok().build();
   }
 
   @DeleteMapping("/{placeId}")
   public ResponseEntity<Void> stampPlace(
-    @PathVariable UUID placeId,
-    @RequestParam(name = "userId") UUID userId // 임시
+      @PathVariable UUID placeId,
+      @AuthenticationPrincipal JidogamUserDetails principal
   ) {
-    stampService.cancelStamp(userId, placeId);
+    stampService.cancelStamp(principal.getId(), placeId);
     return ResponseEntity.noContent().build();
   }
 }
