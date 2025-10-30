11773e8ed7e062044db5bb14d99dd935ca412533
yerimYeon <99879845+yinneu@users.noreply.github.com>
2025-10-30 20:26:57 +0900

    chore: N+1 방지를 위한 @ManyToOne, @OneToOne: FetchType.LAZY 설정 추가

--- src/main/java/region/jidogam/domain/place/entity/Place.java
+++ src/main/java/region/jidogam/domain/place/entity/Place.java
@@ -2,6 +2,7 @@ package region.jidogam.domain.place.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -22,7 +23,7 @@ import region.jidogam.domain.area.entity.Area;
 @Builder
 public class Place extends BaseEntity {
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "area_id", nullable = false)
   private Area area;
 
