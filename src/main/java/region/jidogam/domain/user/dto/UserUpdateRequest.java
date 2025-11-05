11804f7a80e69753a446c41fa5fe1b87c17d43f8
yudility <yudility@gmail.com>
2025-11-05 22:39:22 +0900

    refactor: UserUpdateRequest 필드 Optional 제거
    - UserUpdateRequest의 nickname, password, profileImageUrl 필드를 Optional에서 String 타입으로 변경

--- src/main/java/region/jidogam/domain/user/dto/UserUpdateRequest.java
+++ src/main/java/region/jidogam/domain/user/dto/UserUpdateRequest.java
@@ -1,23 +1,19 @@
 package region.jidogam.domain.user.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import java.util.Optional;
 
 public record UserUpdateRequest(
 
-    @NotBlank
     @Size(min = 3, max = 20, message = "사용자 닉네임은 3 ~ 20글자여야 합니다.")
     @Schema(description = "변경할 사용자 닉네임")
-    Optional<String> nickname,
+    String nickname,
 
-    @NotBlank
     @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
     @Schema(description = "변경할 사용자 비밀번호")
-    Optional<String> password,
+    String password,
 
-    Optional<String> profileImageUrl
+    String profileImageUrl
 
 ) {
 
