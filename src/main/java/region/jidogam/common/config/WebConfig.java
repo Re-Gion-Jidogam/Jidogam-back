6d83911ce9bdf758699eb75716e8f23a48bd0bc0
yudility <yudility@gmail.com>
2025-12-18 21:12:28 +0900

    refactor: 메시지 컨버터 교체 방식 수정
    기존의 MappingJackson2HttpMessageConverter를 탐색 후 MyHttpMessageConverter로 교체하도록 수정.

--- src/main/java/region/jidogam/common/config/WebConfig.java
+++ src/main/java/region/jidogam/common/config/WebConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import region.jidogam.common.converter.MyHttpMessageConverter;
@@ -18,7 +19,13 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-    converters.add(0, new MyHttpMessageConverter(objectMapper));
+    // 기존 MappingJackson2HttpMessageConverter를 찾아서 교체
+    for (int i = 0; i < converters.size(); i++) {
+      if (converters.get(i) instanceof MappingJackson2HttpMessageConverter) {
+        converters.set(i, new MyHttpMessageConverter(objectMapper));
+        break;
+      }
+    }
   }
 
   @Override
